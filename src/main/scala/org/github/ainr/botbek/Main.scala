ebe5f18a5aa9aacbdb0a339328b584142b7822c1
a-khakimov <techlinked@yandex.ru>
2022-02-09 23:41:09 +0500

    Scheduled unsplash statistics to chat

--- src/main/scala/org/github/ainr/botbek/Main.scala
+++ src/main/scala/org/github/ainr/botbek/Main.scala
@@ -1,12 +1,19 @@
 package org.github.ainr.botbek
 
+import cats.effect.kernel.GenTemporal
 import cats.effect.{ExitCode, IO, IOApp}
 import org.github.ainr.botbek.conf.Config
+import org.github.ainr.botbek.infrastructure.Timer
 import org.github.ainr.botbek.tg.bot.BotBek
 import org.github.ainr.botbek.unsplash.module.UnsplashModule
 import org.http4s.blaze.client.BlazeClientBuilder
 
+import scala.concurrent.duration.FiniteDuration
+
 object Main extends IOApp {
+
+  implicit val timer: Timer[IO] =
+    (duration: FiniteDuration) => GenTemporal[IO].sleep(duration)
 
   val app: IO[Unit] = BlazeClientBuilder[IO]
     .resource
@@ -20,6 +27,7 @@ object Main extends IOApp {
         )(
           unsplash.unsplashService
         )
+        _ <- bot.runScheduledTasks().start
         _ <- bot.start()
       } yield ()
     }
--- src/main/scala/org/github/ainr/botbek/tg/bot/BotBek.scala
+++ src/main/scala/org/github/ainr/botbek/tg/bot/BotBek.scala
@@ -3,38 +3,66 @@ package org.github.ainr.botbek.tg.bot
 import cats.Parallel
 import cats.effect.Async
 import cats.syntax.all._
+import org.github.ainr.botbek.infrastructure.Timer
 import org.github.ainr.botbek.tg.conf.TelegramConfig
 import org.github.ainr.botbek.unsplash.service.UnsplashService
 import org.http4s.client.Client
 import telegramium.bots.high.implicits.methodOps
 import telegramium.bots.high.{Api, BotApi, LongPollBot, Methods}
-import telegramium.bots.{ChatIntId, Message}
+import telegramium.bots.{ChatId, ChatIntId, Message}
+import org.github.ainr.botbek.infrastructure.syntax.schedulerSyntax
+
+import java.time.LocalTime
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 
 final class BotBek[
-    F[_]: Async: Parallel
+    F[_]: Async: Parallel: Timer
 ](
     api: Api[F]
 )(
     unsplashService: UnsplashService[F]
 ) extends LongPollBot[F](api) {
 
+  def runScheduledTasks(): F[List[Unit]] = tasks.flatMap {
+    tasks =>
+      val now = LocalTime.now()
+      tasks.traverse { task =>
+        if (task.time.isAfter(now) && task.time.isBefore(now.plusMinutes(1)))
+          sendUnsplashStat(task.chatId, task.user)
+        else ().pure[F]
+      }
+  }
+    .every(1 minute)
+
+  private def sendUnsplashStat(chatId: ChatId, unsplashUser: String): F[Unit] =
+    for {
+      stat <- unsplashService.getUserStatistics(unsplashUser)
+      lastDayDownloads =
+        stat.downloads.historical.values.reverse.headOption.map(
+          _.value.toString
+        ).getOrElse("неизвестно")
+      lastDayViews = stat.views.historical.values.reverse.headOption.map(
+        _.value.toString
+      ).getOrElse("неизвестно")
+      _ <- Methods
+        .sendMessage(
+          chatId = chatId,
+          text =
+            s"""
+            |У ${stat.username} сегодня
+            |скачиваний: $lastDayDownloads
+            |просмотров: $lastDayViews
+            |""".stripMargin
+        )
+        .exec(api)
+    } yield ()
+
   override def onMessage(msg: Message): F[Unit] = for {
-    stat <- unsplashService.getUserStatistics("ainr")
-    lastDayDownloads = stat.downloads.historical.values.headOption.map(
-      _.value.toString
-    ).getOrElse("неизвестно")
-    lastDayViews = stat.views.historical.values.headOption.map(
-      _.value.toString
-    ).getOrElse("неизвестно")
     _ <- Methods
       .sendMessage(
         chatId = ChatIntId(msg.chat.id),
-        text =
-          s"""
-             |У ${stat.username} сегодня
-             |скачиваний: $lastDayDownloads
-             |просмотров: $lastDayViews
-             |""".stripMargin
+        text = s"Привет, брат."
       )
       .exec(api)
   } yield ()
@@ -43,7 +71,7 @@ final class BotBek[
 object BotBek {
 
   def make[
-      F[_]: Async: Parallel
+      F[_]: Async: Parallel: Timer
   ](
       conf: TelegramConfig,
       httpClient: Client[F]
