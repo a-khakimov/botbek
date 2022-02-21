506494f4e7e020fff39907b9dd494860aec19929
a-khakimov <techlinked@yandex.ru>
2022-02-21 22:46:02 +0500

    Update tasks

--- src/main/scala/org/github/ainr/botbek/schedule/package.scala
+++ src/main/scala/org/github/ainr/botbek/schedule/package.scala
@@ -16,11 +16,6 @@ package object schedule {
 
   // yep. this is hard code, bro
   def tasks[F[_]: Applicative]: F[List[Task]] = List(
-    Task("ainr", ChatIntId(174861972), LocalTime.of(6, 0)),
-    Task("ainr", ChatIntId(174861972), LocalTime.of(9, 0)),
-    Task("ainr", ChatIntId(174861972), LocalTime.of(12, 0)),
-    Task("ainr", ChatIntId(174861972), LocalTime.of(15, 0)),
-    Task("ainr", ChatIntId(174861972), LocalTime.of(18, 0)),
-    Task("ainr", ChatIntId(174861972), LocalTime.of(21, 0))
+    Task("ainr", ChatIntId(174861972), LocalTime.of(18, 0))
   ).pure[F]
 }
