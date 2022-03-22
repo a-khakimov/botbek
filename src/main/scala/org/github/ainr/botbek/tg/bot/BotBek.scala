65028f85d50389b286deca643d121947afa48a94
a-khakimov <techlinked@yandex.ru>
2022-03-23 00:06:05 +0500

    draft of logging request context

--- src/main/scala/org/github/ainr/botbek/tg/bot/BotBek.scala
+++ src/main/scala/org/github/ainr/botbek/tg/bot/BotBek.scala
@@ -1,8 +1,11 @@
 package org.github.ainr.botbek.tg.bot
 
 import cats.Parallel
-import cats.effect.Async
-import cats.syntax.all._
+import cats.effect.{Async, Sync}
+import cats.syntax.all.*
+import cats.effect.*
+import cats.implicits.*
+import cats.{Monad, MonadThrow, ~>}
 import org.github.ainr.botbek.tg.conf.TelegramConfig
 import org.http4s.client.Client
 import telegramium.bots
@@ -19,41 +22,86 @@ trait BotBek[F[_]] {
   def sendPhoto(chatId: ChatId, photo: IFile): F[bots.Message]
 }
 
-object BotBek {
+object BotBek:
+
+  object context {
+    type Contextual[T] = RequestContext ?=> T
+    final case class RequestContext(requestId: String)
+
+    object RequestContextual:
+      def withContext[F[_]: Sync](f: Contextual[F[Unit]]): F[Unit] = {
+        val context: RequestContext =
+          RequestContext(java.util.UUID.randomUUID.toString)
+        f(
+          using
+          context
+        )
+      }
+
+    import org.typelevel.log4cats.Logger
+
+    class ContextualLogger[F[_]](logger: Logger[F]):
+
+      private def contextual(message: => String): Contextual[String] =
+        s"${summon[RequestContext].requestId} - $message"
+
+      def error(message: => String): Contextual[F[Unit]] =
+        logger.error(contextual(message))
+      def warn(message: => String): Contextual[F[Unit]] =
+        logger.warn(contextual(message))
+      def info(message: => String): Contextual[F[Unit]] =
+        logger.info(contextual(message))
+      def debug(message: => String): Contextual[F[Unit]] =
+        logger.debug(contextual(message))
+      def trace(message: => String): Contextual[F[Unit]] =
+        logger.trace(contextual(message))
+  }
 
   def make[F[_]: Async: Parallel](
       conf: TelegramConfig,
       httpClient: Client[F]
   ): LongPollBot[F] with BotBek[F] = {
+
+    import context._
+    import context.RequestContextual._
+
+    import org.typelevel.log4cats.slf4j.Slf4jLogger
+    val logger: ContextualLogger[F] =
+      new ContextualLogger(Slf4jLogger.getLogger[F])
+
     val api: Api[F] = BotApi(
-      httpClient,
+      http = httpClient,
       baseUrl = s"${conf.url}/bot${conf.token}"
     )
-    new LongPollBot[F](api) with BotBek[F] {
 
-      override def onMessage(msg: Message): F[Unit] = {
-        Methods.sendMessage(
-          chatId = ChatIntId(msg.chat.id),
-          text = s"Привет, брат. Да, я работаю."
-        ).exec(api).as(())
+    new LongPollBot[F](api) with BotBek[F]:
+
+      def handle(message: Message): Contextual[F[Unit]] = {
+        for
+          _ <- logger.info(s"Message from ${message.chat.id}")
+          _ <- Methods.sendMessage(
+            chatId = ChatIntId(message.chat.id),
+            text = s"Привет, брат. Да, я работаю."
+          ).exec(api)
+        yield ()
+      }
+
+      override def onMessage(message: Message): F[Unit] = withContext {
+        handle(message)
       }
 
       override def sendMessage[Message](
           chatId: ChatId,
           text: String
-      ): F[bots.Message] = {
+      ): F[bots.Message] =
         Methods.sendMessage(
           chatId = chatId,
           text = text
         ).exec(api)
-      }
 
-      override def sendPhoto(chatId: ChatId, photo: IFile): F[Message] = {
+      override def sendPhoto(chatId: ChatId, photo: IFile): F[Message] =
         Methods.sendPhoto(
           chatId = chatId,
           photo = photo
         ).exec(api)
-      }
-    }
   }
-}
